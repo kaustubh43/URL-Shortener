1d173569ecbe224c454ebcb4cc04882f9b92bd40
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2026-05-03 23:59:29 +0530

    Make UrlMapping Model

--- src/main/java/org/urlshortener/urlshortener/models/UrlMapping.java
+++ src/main/java/org/urlshortener/urlshortener/models/UrlMapping.java
@@ -1,4 +1,31 @@
 package org.urlshortener.urlshortener.models;
 
-public class UrlMapping {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(
+        name = "url_mapping",
+        indexes = {
+                @Index(name = "idx_short_url", columnList = "short_url")
+        }
+)
+public class UrlMapping extends BaseModel {
+
+    @Column(name = "long_url", nullable = false, length = 2048)
+    String longUrl;
+
+    @Column(name = "short_url", nullable = false, length = 2048)
+    String shortUrl;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
 }
