767ae16bc3aa156551e8421d7382d17e46b0d0ef
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2026-05-04 00:29:37 +0530

    Add UrlMappingRepository

---
+++ src/main/java/org/urlshortener/urlshortener/repositories/UrlMappingRepository.java
@@ -0,0 +1,10 @@
+package org.urlshortener.urlshortener.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.urlshortener.urlshortener.models.UrlMapping;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+
+}
